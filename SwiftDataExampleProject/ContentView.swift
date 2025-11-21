9e0a711ad7768cb2cdfe49576f4fe8aa660bd967
Emirhan İpek <emirrhanipek@gmail.com>
2025-11-21 22:14:41 +0300

    Initial Commit

---
+++ SwiftDataExampleProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftDataExampleProject
+//
+//  Created by Emirhan İpek on 21.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftDataExampleProject/SwiftDataExampleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataExampleProjectApp.swift
+//  SwiftDataExampleProject
+//
+//  Created by Emirhan İpek on 21.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataExampleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
